26f14729cc0656743a2c1ffa213ab233908eaf7e
Note5019 <40689551+Note5019@users.noreply.github.com>
2019-12-18 22:41:31 +0700

    Next.JS Tutorial

---
+++ 2-using-shared-components/pages/about.jsx
@@ -0,0 +1,15 @@
+import Layout from '../comps/MyLayout';
+
+export default function About() {
+  return (
+    <Layout>
+      <p>This is the about page</p>
+      <p>This is the about page</p>
+      <p>This is the about page</p>
+      <p>This is the about page</p>
+      <p>This is the about page</p>
+      <p>This is the about page</p>
+
+    </Layout>
+  );
+}
---
+++ 3-create-dynamic-pages/pages/post.jsx
@@ -0,0 +1,36 @@
+// import { useRouter } from 'next/router';
+// import Layout from '../components/MyLayout';
+
+// const Page = () => {
+//   const router = useRouter();
+
+//   return (
+//     <Layout>
+//       <h1>{router.query.title}</h1>
+//       <p>This is the blog post content.</p>
+//     </Layout>
+//   );
+// };
+
+// export default Page;
+
+import { useRouter } from 'next/router';
+import Layout from '../components/MyLayout.jsx';
+
+const Content = () => {
+  const router = useRouter();
+  return (
+    <>
+      <h1>{router.query.title}</h1>
+      <p>This is the blog post content.</p>
+    </>
+  );
+};
+
+const Page = () => (
+  <Layout>
+    <Content />
+  </Layout>
+);
+
+export default Page;
